bb3582bb10d317fa3f2d0baf5d03c8fc2d4227d3
ilhamfazri3rd <ilhamfazri3rd@gmail.com>
2020-06-26 11:10:29 +0700

    baru

--- calibration.js
+++ calibration.js
@@ -1,3 +1,6 @@
+var Service = require('./Service');
+var ServiceRequest = require('./ServiceRequest');
+
 var HMin = document.getElementById("HMin");
 var HMinText = document.getElementById("HMinText");
 var HMax = document.getElementById("HMax");
@@ -21,6 +24,8 @@ var GaussianText = document.getElementById("GaussianText");
 var Morphologi = document.getElementById("Morphologi");
 var MorphologiText = document.getElementById("MorphologiText");
 
+var ipaddress;
+
 HMinText.innerHTML = HMin.value;
 HMaxText.innerHTML = HMax.value;
 SMinText.innerHTML = SMin.value;
@@ -29,6 +34,42 @@ VMinText.innerHTML = VMin.value;
 VMaxText.innerHTML = VMax.value;
 GaussianText.innerHTML = Gaussian.value;
 MorphologiText.innerHTML = Morphologi.value;
+
+console.log(window.location.hash.substring(1));
+ipaddress=window.location.hash.substring(1);
+setWebSocket();
+
+
+
+function main_htmlFun(){
+    window.location.href = 'main.html' + '#' + ipaddress;
+}
+
+    function setWebSocket(){
+        var str1='ws://';
+        var str2=':8080';
+        var combine=str1.concat(ipaddress);
+        var ipaddress2=combine.concat(str2);
+
+        var ros = new ROSLIB.Ros({
+          url : ipaddress2
+        });
+      
+        ros.on('connection', function() {
+        document.getElementById("status").innerHTML = "Connected";
+      });
+      
+      ros.on('error', function(error) {
+        document.getElementById("status").innerHTML = "Error";
+      });
+      
+      ros.on('close', function() {
+        document.getElementById("status").innerHTML = "Closed";
+      }); }
+
+      
+      /**
+
 
 // Update the current slider value (each time you drag the slider handle)
 HMin.oninput = function() {
--- main.js
+++ main.js
@@ -1,1 +1,130 @@
+
+
 document.getElementById('logo').src='Ashwincarra.png';
+
+var ip,ipaddress,ipget;
+ipget=window.location.hash.substring(1);
+console.log(ipget)
+
+if(ipget!=null){
+  var str1='ws://';
+  var str2=':8080';
+  var combine=str1.concat(ipget);
+  ipaddress=combine.concat(str2);
+  document.getElementById("ip_address").value=ipget;
+  setWebSocket();
+}
+
+function calibration_html(){
+  ip=document.getElementById("ip_address");
+  window.location.href = 'calibration.html' + '#' + ip.value;
+}
+
+
+function setIP(){
+  ip=document.getElementById("ip_address");
+  var str1='ws://';
+  var str2=':8080';
+  var combine=str1.concat(ip.value);
+  ipaddress=combine.concat(str2);
+  setWebSocket();
+}
+
+
+function setWebSocket(){
+  
+  var ros = new ROSLIB.Ros({
+    url : ipaddress
+  });
+
+  ros.on('connection', function() {
+  document.getElementById("status").innerHTML = "Connected";
+});
+
+ros.on('error', function(error) {
+  document.getElementById("status").innerHTML = "Error";
+});
+
+ros.on('close', function() {
+  document.getElementById("status").innerHTML = "Closed";
+}); 
+
+
+var txt_longitude= new ROSLIB.Topic({
+ros : ros,
+name : '/gui/longitude',
+messageType : 'std_msgs/Float32'
+});
+
+txt_longitude.subscribe(function(m) {
+document.getElementById("longitude").innerHTML = m.data;
+});
+
+
+var txt_latitude= new ROSLIB.Topic({
+ros : ros,
+name : '/gui/latitude',
+messageType : 'std_msgs/Float32'
+});
+
+txt_latitude.subscribe(function(m) {
+document.getElementById("latitude").innerHTML = m.data;
+});
+
+var txt_altitude= new ROSLIB.Topic({
+ros : ros,
+name : '/gui/altitude',
+messageType : 'std_msgs/Float32'
+});
+
+txt_altitude.subscribe(function(m) {
+document.getElementById("altitude").innerHTML = Math.round(m.data* 100) / 100;
+});
+
+
+
+var txt_mode= new ROSLIB.Topic({
+ros : ros,
+name : '/gui/mode',
+messageType : 'std_msgs/String'
+});
+
+txt_mode.subscribe(function(m) {
+document.getElementById("mode").innerHTML = m.data
+});
+
+var txt_motor_status= new ROSLIB.Topic({
+ros : ros,
+name : '/gui/armed',
+messageType : 'std_msgs/Bool'
+});
+
+txt_motor_status.subscribe(function(m) {
+  if(m.data==true){
+    document.getElementById("motor_status").innerHTML = "Armed"
+  }
+  else{
+    document.getElementById("motor_status").innerHTML = "Disarmed"
+  }
+
+});
+
+
+var txt_lidar_range= new ROSLIB.Topic({
+ros : ros,
+name : '/gui/lidar',
+messageType : 'std_msgs/Float32'
+});
+
+txt_lidar_range.subscribe(function(m) {
+  if(m.data<=0){
+    document.getElementById("lidar").innerHTML = "Unavailable"
+  }
+  else{
+    document.getElementById("motor_status").innerHTML = m.data
+  }
+
+});
+}
+
+  
